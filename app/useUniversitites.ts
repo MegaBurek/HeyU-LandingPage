b26f9f57c56fe609652ac8be7ee05d18d6ec6828
Aleksey <aleksey.kovacevic@visit.com>
2024-04-13 18:49:33 +0200

    Fix build error

--- app/useUniversitites.ts
+++ app/useUniversitites.ts
@@ -1,17 +1,17 @@
-import {useSuspenseQuery} from "@tanstack/react-query"
-import {client} from "./useSupabase"
-
-export const useUniversities = () => {
-    const useUniversitiesKey = "universities"
-
-    return useSuspenseQuery({
-        queryKey: [useUniversitiesKey],
-        queryFn: async () => {
-            const { data: universities } = await client.from("universities").select().eq('enabled', true)
-
-            return {
-                universities
-            }
-        }
-    })
-}
+// import {useSuspenseQuery} from "@tanstack/react-query"
+// import {client} from "./useSupabase"
+//
+// export const useUniversities = () => {
+//     const useUniversitiesKey = "universities"
+//
+//     return useSuspenseQuery({
+//         queryKey: [useUniversitiesKey],
+//         queryFn: async () => {
+//             const { data: universities } = await client.from("universities").select().eq('enabled', true)
+//
+//             return {
+//                 universities
+//             }
+//         }
+//     })
+// }
